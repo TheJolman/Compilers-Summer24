7beb6a12e42928cb488dae6dbf69740710515f4b
josh <joshuaholman5@gmail.com>
2024-06-25 22:56:47 -0700

    done

--- project3/Prog2.cpp
+++ project3/Prog2.cpp
@@ -9,9 +9,13 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <cctype> // std::cctype
+#include <cassert>
 
 using StateTable = std::map<std::string, std::map<std::string, std::string>>;
 using VecStack = std::vector<std::string>;
+using Production = std::pair<std::string, std::string>;
+using Grammar = std::vector<Production>;
 
 // this might be more readable than how I did it in Prog1
 StateTable create_state_table() {
@@ -51,21 +55,37 @@ StateTable create_state_table() {
   return table;
 }
 
+Grammar create_grammar() {
+  Grammar grammar;
+  grammar.push_back(std::make_pair("E", "E+T"));
+  grammar.push_back(std::make_pair("E", "E-T"));
+  grammar.push_back(std::make_pair("E", "T"));
+  grammar.push_back(std::make_pair("T", "T*F"));
+  grammar.push_back(std::make_pair("T", "T/F"));
+  grammar.push_back(std::make_pair("T", "F"));
+  grammar.push_back(std::make_pair("F", "(E)"));
+  grammar.push_back(std::make_pair("F", "i"));
+  return grammar;
+}
 
 void print_state_table(const StateTable &);
 void print_stack(const VecStack &);
 void push_string(std::vector<char> &, const std::string &);
 void print_trace(std::string &, std::string::iterator);
 std::string get_table_action(const StateTable &, const std::string &, const std::string &);
+void reduce(const StateTable &, VecStack &, const Production &);
 
 
 int main(void) {
   StateTable state_table = create_state_table();
+  Grammar grammar = create_grammar();
   /*print_state_table(state_table);*/
 
+  // initial important vars
   std::string input;
   std::string trace = "";
   VecStack stack = {};
+
   stack.push_back("0");
   std::cout << "Enter input string: ";
   std::getline(std::cin, input);
@@ -90,37 +110,78 @@ int main(void) {
   // const iterator to string
   auto curr_symbol = input.begin(); 
   print_trace(input, curr_symbol);
+
+  // vars used in loop
   int iteration = 0;
   std::string state;
   std::string incoming_token;
   std::string table_action;
+  int rule_num;
+  std::string new_state;
+  Production production;
 
-  while (!stack.empty()) {
-    iteration++;
-    std::cout << "\n---Currently on iteration: " << iteration << "---\n";
+  try {
 
-    // this needs to ignore whitespace to work
-    while (std::isspace(*curr_symbol)) {
-      curr_symbol++;
+    while (!stack.empty()) {
+      iteration++;
+      std::cout << "\n---Currently on iteration: " << iteration << "---\n";
+      print_stack(stack);
+      print_trace(input, curr_symbol);
+
+      // this needs to ignore whitespace to work
+      while (std::isspace(*curr_symbol)) {
+        curr_symbol++;
+      }
+
+      state = stack.back();
+      incoming_token = *curr_symbol;
+      table_action = get_table_action(state_table, state, incoming_token);
+
+      switch (table_action.front()) {
+        case 'S':
+          /*std::cout << "in case 'S'\n";*/
+          // shift
+          stack.push_back(incoming_token);
+          new_state = table_action.substr(1);
+          stack.push_back(new_state); // append state num 
+          curr_symbol++;
+          std::cout << "Pushed " << incoming_token << " and " << new_state << " to stack.\n";
+          break;
+        case 'R':
+          /*std::cout << "in case 'R'\n";*/
+          // reduce
+          /*std::cout << "before converting rule_num to int\n";*/
+          rule_num = std::stoi(table_action.substr(1)) - 1;
+          /*std::cout << "after converting rule_num to int\n";*/
+          if (grammar.size() > rule_num) {
+            production = grammar[rule_num];
+          } else {
+            std::cerr << "rule_num out of bounds.\n";
+            exit(1);
+          }
+          std::cout << "Using rule " << rule_num << " which is " << production.first <<
+            "->" << production.second << "\n";
+          reduce(state_table, stack, production);
+          break;
+        case 'a':
+          // accept state
+          std::cout << "String accepted!\n";
+          exit(0);
+        default: 
+          // error msg
+          std::cout << "Error: something went wrong bruv.\n";
+          print_stack(stack);
+          print_trace(input, curr_symbol);
+          exit(1); 
+      }
     }
 
-    state = stack.back();
-    incoming_token = *curr_symbol;
-    table_action = get_table_action(state_table, state, incoming_token);
-
-    switch (table_action.front()) {
-      case 's': // shift
-               break;
-      case 'r': // reduce
-               break;
-      case 'a': // accept state
-               break;
-      default: exit(1);
-    }
-
-    print_stack(stack);
+  } catch (const std::bad_alloc& e) {
+    std::cerr << "mem allocation failed: " << e.what() << "\n";
+    std::cerr << "stack size: " << stack.size() << "\n";
     print_trace(input, curr_symbol);
   }
+
   return 0;
 }
 
@@ -148,9 +209,9 @@ void print_stack(const VecStack &stack) {
 }
 
 void push_string(std::vector<char> &stack, const std::string &str) {
+  std::cout << "in push_string\n";
   // string const_reverse_iterator
   for (auto rit = str.crbegin(); rit != str.crend(); ++rit) {
-    /*if (*rit != '@')*/
     stack.push_back(*rit);
   }
 }
@@ -167,12 +228,33 @@ void print_trace(std::string &str, std::string::iterator it) {
 }
 
 std::string get_table_action(const StateTable &table, const std::string &state, const std::string &symbol) {
+  /*std::cout << "in get_table_action\n";*/
   auto state_it = table.find(state);
   if (state_it != table.end()) {
     auto symbol_it = state_it->second.find(symbol);
     if (symbol_it != state_it->second.end()) {
+      /*std::cout << "about to return...\n";*/
       return symbol_it->second;
     }
   }
-  return "Error";
+  std::cout << "Error: No table entry for [" << state << ", " << symbol << "]\n";
+  exit(1);
+  /*return "Error";*/
+}
+
+void reduce(const StateTable &table, VecStack &stack, const Production &production) {
+  /* If the RHS contains k symbols, pop the top k state-symbol pairs off the stack
+   * Note the state on top of the stack
+   * Enter go-to part of the table at [stack.back(), lhs], then push lhs and 
+   * the entry found in the table to the stack */
+  std::cout << "in reduce\n";
+  auto [lhs, rhs] = production;
+  size_t elements_to_erase = 2 * rhs.length();
+  stack.erase(stack.end() - elements_to_erase, stack.end());
+  std::string table_entry = get_table_action(table, stack.back(), lhs);
+  std::cout << "pushing: " << lhs << "\n";
+  stack.push_back(lhs);
+  std::cout << "pushing: " << table_entry << "\n";
+  stack.push_back(table_entry);
+
 }
